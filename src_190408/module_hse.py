cd96ee4ab0f361ca9d9a16cc29082b6050731a44
Yong Youn <yybbyb@snu.ac.kr>
2019-05-15 18:04:13 +0900

    Low gap

--- src_190408/module_hse.py
+++ src_190408/module_hse.py
@@ -2,20 +2,39 @@
 ### Date: 2018-12-05			###
 ### yybbyb@snu.ac.kr			###
 ###########################################
+from module_band import *
+from module_vector import *
+from module_vasprun import *
+
 def make_kpts_for_oneshot(kpt_rlx_file,kpt_band_file,target):
 	with open(kpt_rlx_file,'r') as kpt_rlx_inp:
-		kpt_rlx = kpt_rlx_inp.readlines()
+		lines = kpt_rlx_inp.readlines()
+	nkpt = int(lines[1].split()[0])
+	kpt_total = []
+	for line in lines[3:3+nkpt]:
+		kpt_total.append([float(x) for x in line.split()[0:4]])
+
 	with open(kpt_band_file,'r') as kpt_band_inp:
-		kpt_band = kpt_band_inp.readlines()
+		lines = kpt_band_inp.readlines()
+	kpt_band = []
+	for line in lines[3:]:
+		if line.split() > 3:
+			kpt_band.append([float(x) for x in line.split()[0:4]])
+
+	for i in range(len(kpt_band)):
+		overlap_sw = 0
+		for j in range(len(kpt_total)):
+			if abs(kpt_band[i][0]-kpt_total[j][0]) + abs(kpt_band[i][1]-kpt_total[j][1]) + abs(kpt_band[i][2]-kpt_total[j][2]) < 0.0000001:
+				overlap_sw = 1
+		if overlap_sw == 0:
+			kpt_total.append(kpt_band[i])
+
 	with open(target+'/KPOINTS','w') as out:
 		out.write('kpt for hse oneshot\n')
-		nkpt = int(kpt_rlx[1].split()[0])
-		out.write('             '+str(nkpt+2)+'\n')
+		out.write('             '+str(len(kpt_total))+'\n')
 		out.write('Reciprocal\n')
-		for i in range(nkpt):
-			out.write(kpt_rlx[3+i])
-		out.write(kpt_band[3])
-		out.write(kpt_band[4])
+		for i in range(len(kpt_total)):
+			out.write('    '+'    '.join(kpt_total[i])+'\n')
 
 def gap_estimation_hse(target,fermi,spin,ncl,KPT,Band,nelect):
 	VBM = []
@@ -144,3 +163,95 @@ def gap_estimation_hse(target,fermi,spin,ncl,KPT,Band,nelect):
 	gap_simple.close()
 	return gap_final
 
+def DOS_ratio_fermi_to_vb(dos_file,fermi_width,vb_range):
+	DOS = []
+	with open(dos_file,'r') as dos_inp:
+		for i in range(5):
+			dos_inp.readline()
+		line = dos_inp.readline().split()
+		npoint = int(line[2])
+		fermi = float(line[3])
+		for i in range(npoint):
+			line = dos_inp.readline().split()
+			if len(line) == 3:
+				DOS.append([float(line[0]),[float(line[1])]])
+			else:
+				DOS.append([float(line[0]),[float(line[1]),float(line[2])]])
+	vb = [0,0]
+	vb_ind = [0,0]
+	mid = [0,0]
+	mid_ind = [0,0]
+
+	if vb_range[0] < 0:
+		vb_range[0] = -vb_range[0]
+		vb_range[1] = -vb_range[1]
+	vb_range = sorted(vb_range)
+
+	for i in range(npoint):
+		if DOS[i][0] > fermi - vb_range[1] and DOS[i][0] < fermi - vb_range[0]:
+			if vb_ind[0] == 0 :
+				vb_ind[0] = i
+			vb_ind[1] = i
+			for j in range(len(DOS[0][1])):
+				vb[j] = vb[j] + DOS[i][1][j]
+
+		elif DOS[i][0] > fermi - fermi_width and DOS[i][0] < fermi + fermi_width:
+			if mid_ind[0] == 0 :
+				mid_ind[0] = i
+			mid_ind[1] = i
+			for j in range(len(DOS[0][1])):
+				mid[j] = mid[j] + DOS[i][1][j]
+ 
+	avg_vb = (vb[0]+vb[len(DOS[0][1])-1])/2./(DOS[vb_ind[1]][0]-DOS[vb_ind[0]][0])
+	avg_mid = (mid[0]+mid[len(DOS[0][1])-1])/2./(DOS[mid_ind[1]][0]-DOS[mid_ind[0]][0])
+	for i in range(1,len(DOS[0][1])-1):
+		avg_vb = avg_vb + vb[i]/(DOS[vb_ind[1]][0]-DOS[vb_ind[0]][0])
+		avg_mid = avg_mid + mid[i]/(DOS[mid_ind[1]][0]-DOS[mid_ind[0]][0])
+	
+	DF_DVB = avg_mid/(avg_vb+0.00001) # avoid to divide zero
+
+	return DF_DVB
+
+def find_extreme_kpt_for_hse(dir_band,e_width,search_space):
+	fermi = float(subprocess.check_output(['head',dir_band+'/DOSCAR','-n','6']).splitlines()[-1].split()[3])
+	spin = subprocess.check_output(['grep','ISPIN',dir_band+'/OUTCAR']).split()[2]
+	ncl = subprocess.check_output(['grep','NONCOL',dir_band+'/OUTCAR']).split()[2]
+	[KPT,Band,nelect] = EIGEN_to_array(dir_band+'/EIGENVAL',spin)
+
+	num_new_kpt = 0
+	new_kpt = []
+
+	extreme_log = []
+
+	axis = poscar_to_axis(dir_band+'/POSCAR')
+	rec_lat = reciprocal_lattice(axis)
+
+	for n in range(len(Band)):
+		for i in range(len(Band[0][0])):
+			for k in range(len(KPT)):
+				if Band[n][k][i] > fermi-e_width and Band[n][k][i] < fermi+e_width :
+					sign = 0
+					ex_sw = 0
+					for k2 in range(len(KPT)):
+						if not k == k2 and dist_vec(KPT[k],KPT[k2],rec_lat) < search_space:
+							if sign == 0:
+								if Band[n][k][i] > Band[n][k2][i]: # top
+									sign = 1 
+								else: # bottom
+									sign = -1
+						
+							if sign*(Band[n][k][i]-Band[n][k2][i]) < 0 : # not extreme point
+								ex_sw = 1
+								break
+					if ex_sw == 0:
+						extreme_log.append([n,k,i])
+
+	reduced_log = []
+	for i in range(len(extreme_log)):
+		if not extreme_log[i][1] in [reduced_log[x][1] for x in range(len(reduced_log))]:
+			reduced_log.append(extreme_log[i])
+
+	with open(dir_band+'/KPT', 'w') as kpt_out:	# File for VBM & CBM k-point position
+		kpt_out.write("Example file\n               0\nReciprocal\n")
+		for i in range(len(reduced_log)):
+			kpt_out.write('    '+'    '.join(KPT[reduced_log[i][1]])+'\t0\n')
